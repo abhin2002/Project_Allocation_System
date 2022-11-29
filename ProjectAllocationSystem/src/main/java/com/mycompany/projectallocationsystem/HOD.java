772270626cef074a87edb1619b0cc6242a5aea05
abhin2002 <abhinajith40@gmail.com>
2022-11-29 13:01:07 +0530

    updated

--- ProjectAllocationSystem/src/main/java/com/mycompany/projectallocationsystem/HOD.java
+++ ProjectAllocationSystem/src/main/java/com/mycompany/projectallocationsystem/HOD.java
@@ -60,6 +60,11 @@ public class HOD extends javax.swing.JFrame {
         jButton1.setText("Professors");
 
         jButton2.setText("Students");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton3.setText("Projects");
 
@@ -152,6 +157,10 @@ public class HOD extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
